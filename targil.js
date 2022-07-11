42b99da43ad7ad95a4f137629e5e2761af3f8acf
SolomonAvraham <Selmon_a@hotmail.com>
2022-07-11 22:57:51 +0300

    update

---
+++ targil.js
@@ -0,0 +1,13 @@
+class User {
+  constructor(fullName, email) {
+    this.fullName = fullName;
+    this.email = email;
+  }
+    
+}
+
+
+let userOne = new User("soli", "ggg@ggg");
+console.log(userOne);
+
+document.write(userOne)
